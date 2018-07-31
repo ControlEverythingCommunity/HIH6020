cc80094df732dd51d02a05d79d14105a8d654bfe
Trey Felton <trey@tomadoh.com>
2018-07-31 13:54:40 -0500

    Update HIH6020.py

--- Python/HIH6020.py
+++ Python/HIH6020.py
@@ -10,6 +10,10 @@ import time
 # Get I2C bus
 bus = smbus.SMBus(1)
 
+# Send measurement request
+bus.write_quick(0x27)
+time.sleep(0.1)
+
 # HIH6020 address, 0x27(39)
 # Read data back from 0x00(00), 4 bytes
 # humidity MSB, humidity LSB, temp MSB, temp LSB
